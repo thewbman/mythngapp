c09b3c9fd4843288b475b0857e0c715e956ef40c
Wes Brown <thewbman@gmail.com>
2019-02-22 16:04:27 -0700

    auto-hide menu sidebar on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  @ViewChild('drawer') drawer: MatSidenav;
+  mobileQuery: MediaQueryList;
   title = 'Myth-ng App';
+
+  constructor(private router: Router,changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+
+this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
+        router.events.subscribe( (event: Event) => {
+
+            if (event instanceof NavigationStart) {
+                if(this.mobileQuery.matches)
+		  this.drawer.close()
+            }
+
+            if (event instanceof NavigationEnd) {
+                // Hide loading indicator
+            }
+
+            if (event instanceof NavigationError) {
+                // Hide loading indicator
+
+                // Present error to user
+                console.log(event.error);
+            }
+        });
+
+    }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
