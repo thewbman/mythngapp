9dd9bee9e443d3b696047feed93a32588e2d9fe8
Wes Brown <thewbman@gmail.com>
2019-05-14 12:00:33 -0600

    initial loading and parsing of status XML

--- src/app/recstatus.pipe.ts
+++ src/app/recstatus.pipe.ts
@@ -10,26 +10,26 @@ export class RecstatusPipe implements PipeTransform {
 
     if (value !== undefined && value !== null) {
       switch (value) {
-case '-8': return 'TunerBusy';
-case '-7': return 'LowDiskSpace';
+case '-8': return 'Tuner Busy';
+case '-7': return 'Low Disk Space';
 case '-6': return 'Cancelled';
 case '-5': return 'Deleted';
 case '-4': return 'Aborted';
 case '-3': return 'Recorded';
 case '-2': return 'Recording';
-case '-1': return 'WillRecord';
+case '-1': return 'Will Record';
 case '0': return 'Unknown';
-case '1': return 'DontRecord';
-case '2': return 'PreviousRecording';
-case '3': return 'CurrentRecording';
-case '4': return 'EarlierShowing';
-case '5': return 'TooManyRecordings';
-case '6': return 'NotListed';
+case '1': return 'Dont Record';
+case '2': return 'Previous Recording';
+case '3': return 'Current Recording';
+case '4': return 'Earlier Showing';
+case '5': return 'Too Many Recordings';
+case '6': return 'Not Listed';
 case '7': return 'Conflict';
-case '8': return 'LaterShowing';
+case '8': return 'Later Showing';
 case '9': return 'Repeat';
 case '10': return 'Inactive';
-case '11': return 'NeverRecord';
+case '11': return 'Never Record';
 
 default: return 'Unknown status: ' + value.toString();
       }
