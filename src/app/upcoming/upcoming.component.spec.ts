781b85d78f38cacbcd2543c585dac5232b776bc9
Wes Brown <thewbman@gmail.com>
2019-05-30 15:50:45 -0600

    unit tests for upcoming

--- src/app/upcoming/upcoming.component.spec.ts
+++ src/app/upcoming/upcoming.component.spec.ts
@@ -65,4 +65,64 @@ describe('UpcomingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('change tab 1', () => {
+    //changing tabs doesnt seem to call onTabChanged event
+    component.tabIndex = 1;
+    expect(component.tabIndex).toBe(1);
+  });
+  it('call onTabChanges to 3', () => {
+    component.tabIndex = 3;
+    component.onTabChanged({index: 3});
+    expect(component.tabIndex).toBe(3);
+  });
+  it('call onTabChanges to 2', () => {
+    component.tabIndex = 2;
+    component.onTabChanged({index: 2});
+    expect(component.tabIndex).toBe(2);
+  });
+  it('call onTabChanges to 1', () => {
+    component.tabIndex = 1;
+    component.onTabChanged({index: 1});
+    expect(component.tabIndex).toBe(1);
+  });
+  it('call onTabChanges to 0', () => {
+    component.tabIndex = -1;
+    component.onTabChanged({index: 0});
+    expect(component.tabIndex).toBe(0);
+  });
+
+  it('select group All', () => {
+    component.onSelectGroup("All");
+    expect(component.tabIndex).toBe(1);
+  });
+  it('select group Conflicting', () => {
+    component.onSelectGroup("Conflicting");
+    expect(component.tabIndex).toBe(1);
+  });
+  it('select group Overrides', () => {
+    component.onSelectGroup("Overrides");
+    expect(component.tabIndex).toBe(1);
+  });
+  it('select group Upcoming', () => {
+    component.onSelectGroup("Upcoming");
+    expect(component.tabIndex).toBe(1);
+  });
+
+  it('select date allDatesString', () => {
+    component.onSelectDate(component.allDatesString);
+    expect(component.tabIndex).toBe(2);
+  });
+  it('select date last date', () => {
+    component.onSelectDate(component.dateList[component.dateList.length-1]);
+    expect(component.tabIndex).toBe(2);
+  });  
+
+
+  it('select a program', () => {
+    component.onSelectGroup("All");
+    component.onSelectDate(component.allDatesString);
+    component.onSelectProgram(component.allPrograms[0]);
+    expect(component.tabIndex).toBe(3);
+  });
 });
